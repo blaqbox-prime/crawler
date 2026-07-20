f8ebe365a2430c5a5e773fc5976c08b70da94402
Karabo Sambo <karabosambo@outlook.com>
2026-07-20 04:58:52 +0200

    Refactor MongoDB client setup to use settings for connection URI and database name; remove unused fetchBooks function.

--- utils/db.py
+++ utils/db.py
@@ -4,14 +4,15 @@ from pymongo import AsyncMongoClient
 from beanie import init_beanie
 from models.Book import Book
 from models.Api_Key import Api_key
+from utils.settings import get_settings
 
 
 # MongoDB client setup
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    client = AsyncMongoClient("mongodb://localhost:27017")
+    client = AsyncMongoClient(get_settings().mongo_uri)
     try:
-        await init_beanie(database=client["filekeepers"], document_models=[Book, Api_key])
+        await init_beanie(database=client[get_settings().mongo_db_name], document_models=[Book, Api_key])
         yield
     except Exception as e:
         print(f"Error during database initialization: {e}")
